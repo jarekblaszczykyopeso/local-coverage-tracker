a5232cfddc9493768b593302c6847c2b9ab6b4fe
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-10-30 16:24:11 +0200

    project initialization

---
+++ src/main/java/com/yopeso/jacocotracker/JacocoTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.yopeso.jacocotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yopeso/jacocotracker/JacocoTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yopeso.jacocotracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JacocoTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
