021600d5e7422f3d19c15027e9b7e52be25522ce
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-10-31 11:16:21 +0200

    project initialization

---
+++ src/main/java/com/yopeso/coveragetracker/CoverageTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.yopeso.coveragetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverageTrackerApplication.class, args);
+	}
+}
