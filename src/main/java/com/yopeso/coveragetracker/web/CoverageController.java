9003f2e1b84fc1365f334398efe95a06c3b6c74a
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-11-09 15:43:54 +0200

    Implement controllers with test

--- src/main/java/com/yopeso/coveragetracker/web/CoverageController.java
+++ src/main/java/com/yopeso/coveragetracker/web/CoverageController.java
@@ -1,12 +1,133 @@
 package com.yopeso.coveragetracker.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.time.LocalDate;
+import java.util.Collections;
+
+/**
+ * Rest controller for the coverage.
+ * There are 2 gets web services, one with data and the other one without date.
+ * Both of them are using the same method.
+ */
 @RestController
+@RequiredArgsConstructor
 public class CoverageController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hi from Spring!";
+//    @Autowired
+//    CoverageService coverageService;
+
+
+    /**
+     * Returns coverage for the given project, branch, build.
+     *
+     * @param projectName project name
+     * @param branchName  branch name
+     * @param buildNumber build number
+     * @return int the last coverage for given parameters (without date) found in db or -1 if is not found
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/projects/{projectName}/branches/{branchName}/builds/{buildNumber}")
+    public int getCoverage(@PathVariable String projectName, @PathVariable String branchName,
+                           @PathVariable String buildNumber) {
+        return getCoverage(projectName, branchName, buildNumber, null);
+    }
+
+    /**
+     * Returns coverage for the given project, branch, build, date.
+     *
+     * @param projectName project name
+     * @param branchName  branch name
+     * @param buildNumber build number
+     * @param date        date in the format yyyy-MM-dd
+     * @return int the last coverage for given parameters (with date) found in db or -1 if is not found
+     */
+
+    @RequestMapping(method = RequestMethod.GET, value = "/projects/{projectName}/branches/{branchName}/builds/{buildNumber}/dates/{date}")
+    public int getCoverageWithDate(@PathVariable String projectName, @PathVariable String branchName,
+                                   @PathVariable String buildNumber, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        return getCoverage(projectName, branchName, buildNumber, date);
+    }
+
+
+    /**
+     * Create the coverage record in db.
+     * When the record is created return 201 status and in header url for get request for the coverage; 204 status otherwise.
+     *
+     * @param projectName  project name
+     * @param branchName   branch name
+     * @param buildNumber  build number
+     * @param coverageJson json containing coverage put into the body of request
+     * @return ResponseEntity;
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/projects/{projectName}/branches/{branchName}/builds/{buildNumber}")
+    public ResponseEntity<?> postCoverage(@PathVariable String projectName, @PathVariable String branchName,
+                                          @PathVariable String buildNumber, @RequestBody CoverageJson coverageJson) {
+        //not saved record to db
+        //TODO change it!!!
+        if (coverageJson.coverage < 0) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/dates/" + LocalDate.now().toString()).build().toUri()).build();
+        }
+    }
+
+    //TODO change it and use the service with null also
+
+    /**
+     * Auxiliary method used in both get web services. Here is the logic.
+     *
+     * @param projectName project name
+     * @param branchName  branch name
+     * @param buildNumber build number
+     * @param date        date in the format yyyy-MM-dd
+     * @return int the last coverage for given parameters (with or without date) found in db or -1 if is not found
+     */
+    private int getCoverage(String projectName, String branchName, String buildNumber, LocalDate date) {
+        if (date != null) {
+            return 77;
+        } else {
+            return -9;
+        }
+    }
+
+}
+
+/**
+ * POJO class for put the coverage in the request body.
+ */
+@Data
+@AllArgsConstructor
+class CoverageJson {
+    int coverage;
+}
+
+/**
+ * Class with the security configuration.
+ */
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    UserDetailsService users() {
+        return new InMemoryUserDetailsManager(Collections.singleton(User.withUsername("user").roles("ADMIN").password("pwd").build()));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
     }
 }
--- src/test/java/com/yopeso/coveragetracker/web/CoverageControllerTest.java
+++ src/test/java/com/yopeso/coveragetracker/web/CoverageControllerTest.java
@@ -3,24 +3,79 @@ package com.yopeso.coveragetracker.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
+import java.time.LocalDate;
+
 import static org.junit.Assert.assertEquals;
 
 
+/**
+ * Tests for coverage controller.
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CoverageControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @LocalServerPort
+    int localPort;
+
+    String postBadPath = "/projects/projectName/branches/branchName/buildsBadPath/buildNumber";
+    String postGoodPath = "/projects/projectName/branches/branchName/builds/buildNumber";
+    String getBadPath = "/projects/projectName/branches/branchName/buildsBadPath/buildNumber";
+    String getNoDatePath = "/projects/projectName/branches/branchName/builds/buildNumber";
+    String getDateExistsPath = "/projects/projectName/branches/branchName/builds/buildNumber/dates/" + LocalDate.now().toString();
+    //doesn't exist the coverage from the future
+    String getDateNoExistsPath = "/projects/projectName/branches/branchName/builds/buildNumber/dates/" + LocalDate.now().plusDays(3).toString();
+
+
+    /**
+     * Testing the requests according to the scenario: post bad path, post one good, get bad path, get no date, get good date, get bad date.
+     * In the response are verified status, location data in header(one case) and integer in body (3 cases)
+     */
+    //TODO change the test according to scenario: post bad path, post one good, get bad path, get no date, get good date, get bad date
     @Test
     public void testController() {
-        ResponseEntity<String> response = this.restTemplate.getForEntity("/hello", String.class);
-        assertEquals("Hi from Spring!", response.getBody());
+        //add authorisation
+        TestRestTemplate restTemplateWithAuth = this.restTemplate.withBasicAuth("user", "pwd");
 
+        //bad path for post - status checked (entity ? / null, because otherwise error)
+        ResponseEntity<?> responsePostBad = restTemplateWithAuth.postForEntity(
+                postBadPath, new CoverageJson(7), null);
+        assertEquals(404, responsePostBad.getStatusCodeValue());
+
+        //good path for post - status and Location in header checked
+        ResponseEntity<?> responsePostGood = restTemplateWithAuth.postForEntity(
+                postGoodPath, new CoverageJson(7), null);
+        assertEquals(201, responsePostGood.getStatusCodeValue());
+        String expectedUri = URI.create("http://localhost:" + localPort + postGoodPath + "/dates/" + LocalDate.now().toString()).toString();
+        assertEquals(expectedUri, responsePostGood.getHeaders().get("Location").get(0));
+
+        //bad path for get - status checked (entity ? / null, because otherwise error)
+        ResponseEntity<?> responseGetBad = restTemplateWithAuth.getForEntity(getBadPath, null);
+        assertEquals(404, responseGetBad.getStatusCodeValue());
+
+        //good path for get with no date - status and result (last coverage) checked
+        ResponseEntity<Integer> responseGetNoDate = restTemplateWithAuth.getForEntity(getNoDatePath, Integer.class);
+        assertEquals(200, responseGetNoDate.getStatusCodeValue());
+        assertEquals(-9, responseGetNoDate.getBody().intValue());
+
+        //good path for get with good date - status and result (last coverage) checked
+        ResponseEntity<Integer> responseGetDateExists = restTemplateWithAuth.getForEntity(getDateExistsPath, Integer.class);
+        assertEquals(200, responseGetDateExists.getStatusCodeValue());
+        assertEquals(77, responseGetDateExists.getBody().intValue());
+
+        //good path for get with not existing date - status and result (last coverage) checked
+        ResponseEntity<Integer> responseGetDateNoExists = restTemplateWithAuth.getForEntity(getDateNoExistsPath, Integer.class);
+        assertEquals(200, responseGetDateNoExists.getStatusCodeValue());
+        //TODO change it to -1 return
+        // assertEquals(77, responseGetDateNoExists.getBody().intValue());
     }
 }
